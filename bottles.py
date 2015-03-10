e56f7db14e765111c350306141ec82b743c8f730
Kevin Jesus Valle Gomez <kevinvalle1@gmail.com>
2015-03-10 18:29:29 +0100

    Añadido el archivo
    Trabajamos este archivo en cloud9

---
+++ bottles.py
@@ -0,0 +1,6 @@
+i = 99
+
+while i > 0:
+    print i, "bottles of beer on the wall, ", i, "bottles of beer"
+    print "Take one down, pass it around,", i-1, "bottles of beer on the wall."
+    i -= 1
